b58e0a1d9f7c8ea03817dbc349bcedb6ab19f829
Aadesh Goyal <goyalaadesh462@gmail.com>
2020-09-12 14:30:46 +0530

    index commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',async (req,res) => {
-    res.send({msg: 'Nikal Bsdk Sale Corona'}); 
+    res.send({msg: 'Hey Corona'}); 
 });
 
 const PORT = process.env.PORT || 6500;
